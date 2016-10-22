dd0890030585422b16850fca0d6084204c38ef4c
Andrew McLean <mclean14@gmail.com>
2016-10-22 15:32:00 -0600

    Storyboard setup

---
+++ GrippedAthletics/View/Common/UIStoryboard.swift
@@ -0,0 +1,16 @@
+//
+//  UIStoryboard.swift
+//  GrippedAthletics
+//
+//  Created by Andrew McLean on 10/22/16.
+//  Copyright © 2016 GrippedAthletics. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    
+    static let main = "Main"
+    static let login = "Login"
+    
+}
---
+++ GrippedAthletics/View/Common/UIViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController.swift
+//  GrippedAthletics
+//
+//  Created by Andrew McLean on 10/22/16.
+//  Copyright © 2016 GrippedAthletics. All rights reserved.
+//
+
+import UIKit
+
+// MARK: Storyboard
+
+extension UIViewController {
+    
+}
