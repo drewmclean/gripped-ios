961c05a8a692028546f56e3faeb5cd19dec6a7f0
Andrew McLean <mclean14@gmail.com>
2016-11-21 07:55:38 -0700

    observe text

--- GrippedAthletics/View/Auth/AuthTextFieldViewController.swift
+++ GrippedAthletics/View/Auth/AuthTextFieldViewController.swift
@@ -11,27 +11,45 @@ import PromiseKit
 
 class AuthTextFieldViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var fieldContainerView: UIView!
+    @IBOutlet weak var fieldContainerViewBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var fieldLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var fieldLabelCenterYConstraint: NSLayoutConstraint!
     
+    private var labelIsShown : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fieldLabel.isHidden = true
         textField.text = ""
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        textField.observe(keyPath: "text").then { (value: String?) -> Void in
+            if value!.isEmpty {
+                self.hideFieldLabel()
+            } else {
+                self.showFieldLabel()
+            }
+        }
     }
     
     // MARK: Animation
     
     func showFieldLabel() {
-        
+        labelIsShown = true
+        fieldLabel.alpha = 0
+        fieldLabel.isHidden = false
+        UIView.animate(withDuration: 0.2, delay: 0, options: [.beginFromCurrentState, .curveEaseInOut], animations: {
+            self.fieldLabel.alpha = 1.0
+            self.fieldLabelCenterYConstraint.constant = -20.0
+            self.fieldContainerView.layoutIfNeeded()
+        }, completion: { (Bool) -> Void in
+            
+        })
     }
     
     func hideFieldLabel() {
-        
+        labelIsShown = false
     }
 
     // MARK: UITextFieldDelegate
