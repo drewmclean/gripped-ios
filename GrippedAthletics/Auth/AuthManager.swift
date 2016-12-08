067bd82e0d507086671ea0605a79ab93e7a8886d
Andrew McLean <mclean14@gmail.com>
2016-12-07 21:24:29 -0700

    facebook login

--- GrippedAthletics/Auth/AuthManager.swift
+++ GrippedAthletics/Auth/AuthManager.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 import FacebookCore
 import FacebookLogin
 
@@ -16,5 +17,22 @@ class AuthManager: NSObject {
     
     var isAuthenticated : Bool {
         return false
+    }
+    
+    var currentUser : FIRUser?
+             
+    func signIn(withEmail email:String, andPassword password: String) -> Void {
+        
+    }
+    
+    func signIn(withFacebookAccessToken facebookAccessToken: String) -> Void {
+        let credential = FIRFacebookAuthProvider.credential(withAccessToken: facebookAccessToken)
+        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+    
+        if let e = error {
+            self.showErrorAlert(title: "Sign In Error", message: e.localizedDescription)
+            return
+        }
+    
     }
 }
