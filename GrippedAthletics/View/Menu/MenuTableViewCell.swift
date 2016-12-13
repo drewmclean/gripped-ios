bc27facbe9151868078c8f75890f009381e104ab
Andrew Mclean <mclean14@gmail.com>
2016-12-13 15:16:12 -0700

    working on left menu

---
+++ GrippedAthletics/View/Menu/MenuTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MenuTableViewCell.swift
+//  GrippedAthletics
+//
+//  Created by Andrew Mclean on 12/13/16.
+//  Copyright © 2016 GrippedAthletics. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+    
+    static let reuseIdentifier = "MenuTableViewCellID"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- GrippedAthletics/View/Menu/MenuViewController.swift
+++ GrippedAthletics/View/Menu/MenuViewController.swift
@@ -16,6 +16,22 @@ protocol MenuViewControllerDelegate {
     func menuDidChange(menuOption : MenuOption)
 }
 
+enum MenuItemTyoe : Int {
+    case profile, biometrics, climbs
+}
+
+struct MenuItem {
+    var itemType : MenuItemTyoe
+    var label : String
+    var icon : UIImage
+    
+    init(type:MenuItemTyoe, label:String, icon:UIImage) {
+        self.itemType = type
+        self.label = label
+        self.icon = icon
+    }
+}
+
 class MenuViewController: UIViewController {
     
     var delegate : MenuViewControllerDelegate?
@@ -27,10 +43,31 @@ class MenuViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     
+    lazy var menuItems : [MenuItem] = {
+        let items = [MenuItem]()
+        items.append(MenuItem(type: .profile, label: "Profile", icon: UIImage.profile)
+        return items
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.yellow
+        menuTableView.estimatedRowHeight = 44
+        menuTableView.rowHeight = 44
+        menuTableView.dataSource = self
+        menuTableView.delegate = self
     }
     
 }
+
+extension MenuViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+}
+
+extension MenuViewController : UITableViewDelegate {
+    
+}
+
