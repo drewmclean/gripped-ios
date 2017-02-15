c65969f49041c647a0efe92ca7046ef67f8eee3e
Andrew McLean <mclean14@gmail.com>
2017-02-14 18:38:07 -0700

    more things

---
+++ Pods/Target Support Files/IGColorPicker/IGColorPicker-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double IGColorPickerVersionNumber;
+FOUNDATION_EXPORT const unsigned char IGColorPickerVersionString[];
+
---
+++ Pods/Target Support Files/Pods-GrippedAthleticsUITests/Pods-GrippedAthleticsUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GrippedAthleticsUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GrippedAthleticsUITestsVersionString[];
+
